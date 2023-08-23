e7af7755a6dfbdd3cd8097074b5602b10967d5ec
codingMakeOver <132986536+codingMakeOver@users.noreply.github.com>
2023-08-23 18:06:16 +0530

    Update Program.cs

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -5,5 +5,6 @@ for (int i = 0; i < 10000; i++)
     Thread.Sleep(500);
     Console.WriteLine("Hello, World!");
 }
+
 //  docker image build --no-cache -t dotnet6consoleapp:v2 .
 //docker run --rm --platform=windows --env=DOTNET_RUNNING_IN_CONTAINER=true -d --name dotnetcontainerconsoleapp doetnet6consoleapp:v2
