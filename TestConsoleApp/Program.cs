85422bbadd328ea7414078e4af488ec0e871bcd0
Rahul Sen <rahul.sen@mindfiresolutions.com>
2023-08-23 15:37:36 +0530

    Add project files.

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Sockets;
+
+for (int i = 0; i < 10000; i++)
+{
+    Thread.Sleep(500);
+    Console.WriteLine("Hello, World!");
+}
+//  docker image build --no-cache -t dotnet6consoleapp:v2 .
+//docker run --rm --platform=windows --env=DOTNET_RUNNING_IN_CONTAINER=true -d --name dotnetcontainerconsoleapp doetnet6consoleapp:v2
